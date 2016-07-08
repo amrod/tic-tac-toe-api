8883537ba49ed7a07c865b513987808e6fa6157d
Amaury Rodriguez <amaury.rodriguez@dealertrack.com>
2016-07-08 03:24:11 -0400

    Initial commit

---
+++ models.py
@@ -0,0 +1,241 @@
+"""models.py - This file contains the class definitions for the Datastore
+entities used by the Game. Because these classes are also regular Python
+classes they can include methods (such as 'to_form' and 'new_game')."""
+
+import random
+from datetime import date
+from protorpc import messages
+from google.appengine.ext import ndb
+
+
+class User(ndb.Model):
+    """User profile"""
+    name = ndb.StringProperty(required=True)
+    email = ndb.StringProperty()
+
+
+class Game(ndb.Model):
+    """Tic Tac Toe Game object
+
+    The grid is represented by individual cells, numbered as follows:
+
+     1 | 2 | 3
+    -----------
+     4 | 5 | 6
+    -----------
+     7 | 8 | 9
+
+     by fields cell_1 .. cell_9.
+
+     Each cell can have the possible values of -1 (empty),
+     0 (O), or 1 (X).
+
+     player1 is automatically assigned Xs, player2 is Os.
+    """
+
+    game_over = ndb.BooleanProperty(required=True, default=False)
+    player2 = ndb.KeyProperty(required=False, kind='User')
+    next_turn = ndb.KeyProperty(required=True, kind='User')
+    winner = ndb.KeyProperty(required=False, kind='User')
+    cancelled = ndb.BooleanProperty(required=False, default=False)
+    cell_1 = ndb.IntegerProperty(required=False, default=-1)
+    cell_2 = ndb.IntegerProperty(required=False, default=-1)
+    cell_3 = ndb.IntegerProperty(required=False, default=-1)
+    cell_4 = ndb.IntegerProperty(required=False, default=-1)
+    cell_5 = ndb.IntegerProperty(required=False, default=-1)
+    cell_6 = ndb.IntegerProperty(required=False, default=-1)
+    cell_7 = ndb.IntegerProperty(required=False, default=-1)
+    cell_8 = ndb.IntegerProperty(required=False, default=-1)
+    cell_9 = ndb.IntegerProperty(required=False, default=-1)
+
+    @property
+    def player1(self):
+        return self.key.parent()
+
+    @property
+    def grid(self):
+        """Returns the grid represented as a list of lists"""
+        l = [[], [], []]
+        i = 1
+        for j in range(3):
+            for k in range(3):
+                l[j].append(getattr(self, 'cell_{}'.format(i)))
+                i += 1
+        return l
+
+    @grid.setter
+    def grid(self, grid_list):
+        """Store grid in database"""
+        i = 1
+        for j in range(3):
+            for k in range(3):
+                setattr(self, 'cell_{}'.format(i), grid_list[j][k])
+                i += 1
+
+    def cell_names(self):
+        for i in range(1, 10):
+            attr_name = 'cell_{}'.format(i)
+            yield attr_name
+
+    def get_player_symbol(self, user_key):
+        if self.player1 == user_key:
+            return 1
+        elif self.player2 == user_key:
+            return 0
+        else:
+            raise ValueError('User not in this game.')
+
+    @classmethod
+    def new_game(cls, player1_key, player2_key=None):
+        """Creates and returns a new game"""
+
+        game = Game(parent=player1_key,
+                    player2=player2_key,
+                    next_turn=player1_key,
+                    game_over=False)
+        game.put()
+        return game
+
+    def set_player2(self, user_key):
+        self.player2 = user_key
+        self.put()
+
+    def set_position(self, position, user_key):
+        symbol = self.get_player_symbol(user_key)
+        attr_name = 'cell_{}'.format(position)
+        setattr(self, attr_name, symbol)
+
+    def cancel_game(self):
+        self.cancelled = True
+
+    def get_number_of_moves(self, user_key):
+        symbol = self.get_player_symbol(user_key)
+        total = 0
+        for attr_name in self.cell_names():
+            if getattr(self, attr_name) == symbol:
+                total += 1
+        return total
+
+    def to_form(self, message=''):
+        """Returns a GameForm representation of the Game"""
+        form = GameForm()
+        form.urlsafe_key = self.key.urlsafe()
+        form.player1_name = self.player1.get().name
+        form.player2_name = self.player2.get().name if self.player2 else None
+        form.game_over = self.game_over
+        form.message = message
+        form.next_turn = self.next_turn.get().name
+
+        for attr_name in self.cell_names():
+            setattr(form, attr_name, getattr(self, attr_name))
+
+        return form
+
+    def end_game(self, winner):
+        """Ends the game - if won is True, the player won. - if won is False,
+        the player lost."""
+        self.game_over = True
+        self.winner = winner
+        self.put()
+        loser = self.player2 if winner == self.player1 else self.player1
+
+        # Add the game to the score 'board'
+        score = Score(parent=self.key, date=date.today(),
+                      winner=winner, winner_name=winner.get().name,
+                      loser=loser, loser_name=loser.get().name,
+                      winner_moves=self.get_number_of_moves(winner))
+        score.put()
+
+
+class Score(ndb.Model):
+    """Score object"""
+    winner = ndb.KeyProperty(required=True, kind='User')
+    winner_name = ndb.StringProperty(required=True)
+    loser = ndb.KeyProperty(required=True, kind='User')
+    loser_name = ndb.StringProperty(required=True)
+    date = ndb.DateProperty(required=True)
+    winner_moves = ndb.IntegerProperty(required=True)
+
+    @ndb.tasklet
+    def get_winner_name(self):
+        user = yield self.winner.get_async()
+        raise ndb.Return(user.name)
+
+    @ndb.tasklet
+    def get_loser_name(self):
+        user = yield self.loser.get_async()
+        raise ndb.Return(user.name)
+
+    @classmethod
+    def query_user(cls, user_key):
+        return cls.query(ndb.OR(cls.winner == user_key,
+                                cls.loser == user_key))
+
+    def to_form(self):
+        return ScoreForm(user_name=self.winner_name,
+                         date=str(self.date), moves=self.winner_moves)
+
+
+class GameForm(messages.Message):
+    """GameForm for outbound game state information"""
+    urlsafe_key = messages.StringField(1, required=True)
+    game_over = messages.BooleanField(2, required=True)
+    message = messages.StringField(3, required=True)
+    player1_name = messages.StringField(4, required=True)
+    player2_name = messages.StringField(5, required=False)
+    next_turn = messages.StringField(6, required=True)
+    cell_1 = messages.IntegerField(7, required=True)
+    cell_2 = messages.IntegerField(8, required=True)
+    cell_3 = messages.IntegerField(9, required=True)
+    cell_4 = messages.IntegerField(10, required=True)
+    cell_5 = messages.IntegerField(11, required=True)
+    cell_6 = messages.IntegerField(12, required=True)
+    cell_7 = messages.IntegerField(13, required=True)
+    cell_8 = messages.IntegerField(14, required=True)
+    cell_9 = messages.IntegerField(15, required=True)
+
+
+class GameForms(messages.Message):
+    """Return multiple GameForm """
+    items = messages.MessageField(GameForm, 1, repeated=True)
+
+
+class UserNameForm(messages.Message):
+    """User name"""
+    player_1 = messages.StringField(1, required=True)
+    player_2 = messages.StringField(2, required=False)
+
+class MakeMoveForm(messages.Message):
+    """Used to make a move in an existing game"""
+    user_name = messages.StringField(1, required=True)
+    position = messages.IntegerField(2, required=True)
+
+
+class ScoreForm(messages.Message):
+    """ScoreForm for outbound Score information"""
+    user_name = messages.StringField(1, required=True)
+    date = messages.StringField(2, required=True)
+    moves = messages.IntegerField(3, required=True)
+
+
+class ScoreForms(messages.Message):
+    """Return multiple ScoreForm"""
+    items = messages.MessageField(ScoreForm, 1, repeated=True)
+
+
+class RankingForm(messages.Message):
+    """Outbound ranking information"""
+    user_name = messages.StringField(1, required=True)
+    rank = messages.IntegerField(2, required=True)
+    performance = messages.FloatField(3, required=True)
+
+
+class RankingForms(messages.Message):
+    """Return multiple RankingForm"""
+    items = messages.MessageField(RankingForm, 1, repeated=True)
+
+
+class StringMessage(messages.Message):
+    """StringMessage-- outbound (single) string message"""
+    message = messages.StringField(1, required=True)
+
---
+++ tic_tac_toe.py
@@ -0,0 +1,347 @@
+# -*- coding: utf-8 -*-`
+"""api.py - Create and configure the Game API exposing the resources.
+This can also contain game logic. For more complex games it would be wise to
+move game logic to another file. Ideally the API will be simple, concerned
+primarily with communication to/from the API's users."""
+
+from __future__ import division
+import logging
+
+import endpoints
+from google.appengine.ext import ndb
+from protorpc import remote, messages, message_types
+from google.appengine.api import memcache
+from google.appengine.api import taskqueue
+
+from models import User, Game, Score, RankingForms, RankingForm
+from models import (
+    StringMessage,
+    GameForm,
+    GameForms,
+    MakeMoveForm,
+    ScoreForms,
+    UserNameForm
+)
+from utils import get_by_urlsafe
+
+NEW_GAME_REQUEST = endpoints.ResourceContainer(UserNameForm)
+JOIN_GAME_REQUEST = endpoints.ResourceContainer(
+    UserNameForm,
+    urlsafe_game_key=messages.StringField(1),
+)
+GET_GAME_REQUEST = endpoints.ResourceContainer(
+    urlsafe_game_key=messages.StringField(1),
+)
+MAKE_MOVE_REQUEST = endpoints.ResourceContainer(
+    MakeMoveForm,
+    urlsafe_game_key=messages.StringField(1),
+)
+USER_REQUEST = endpoints.ResourceContainer(
+    user_name=messages.StringField(1),
+    email=messages.StringField(2)
+)
+GET_USER_GAMES_REQUEST = endpoints.ResourceContainer(
+    user_name=messages.StringField(1)
+)
+
+
+MEMCACHE_MOVES_REMAINING = 'MOVES_REMAINING'
+API_EXPLORER_CLIENT_ID = endpoints.API_EXPLORER_CLIENT_ID
+
+
+@endpoints.api(name='tic_tac_toe',
+               version='v1',
+               allowed_client_ids=[API_EXPLORER_CLIENT_ID])
+class TicTacToeApi(remote.Service):
+    """Game API"""
+    @endpoints.method(request_message=USER_REQUEST,
+                      response_message=StringMessage,
+                      path='user',
+                      name='create_user',
+                      http_method='POST')
+    def create_user(self, request):
+        """Create a User. Requires a unique username"""
+        if User.query(User.name == request.user_name).get():
+            raise endpoints.ConflictException(
+                    'A User with that name already exists!')
+        user = User(name=request.user_name, email=request.email)
+        user.put()
+        return StringMessage(message='User {} created!'.format(
+                request.user_name))
+
+    @endpoints.method(request_message=NEW_GAME_REQUEST,
+                      response_message=GameForm,
+                      path='game',
+                      name='new_game',
+                      http_method='POST')
+    def new_game(self, request):
+        """Creates new game"""
+        player1 = User.query(User.name == request.player_1).get()
+
+        if not player1:
+            raise endpoints.NotFoundException(
+                    'No user named "{}" was found.'.format(request.player_1))
+
+        player2 = User.query(User.name == request.player_2).get()
+
+        if request.player_2 and not player2:
+            raise endpoints.NotFoundException(
+                    'No user named "{}" was found.'.format(request.player_1))
+
+        try:
+            game = Game.new_game(player1.key, player2.key if player2 else None)
+        except ValueError:
+            raise endpoints.BadRequestException('**Some error**')
+
+        # Use a task queue to update the average attempts remaining.
+        # This operation is not needed to complete the creation of a new game
+        # so it is performed out of sequence.
+        #taskqueue.add(url='/tasks/cache_average_attempts')
+
+        return game.to_form('Good luck playing Tic Tac Toe!')
+
+    @endpoints.method(request_message=JOIN_GAME_REQUEST,
+                      response_message=GameForm,
+                      path='game/{urlsafe_game_key}/join',
+                      name='join_game',
+                      http_method='PUT')
+    def join_game(self, request):
+        """Joins user to game as player2"""
+        user = User.query(User.name == request.user_name).get()
+        if not user:
+            raise endpoints.NotFoundException(
+                'A User with that name does not exist!')
+
+        game = get_by_urlsafe(request.urlsafe_game_key, Game)
+
+        if not game:
+            raise endpoints.NotFoundException('Game not found!')
+
+        if user.key == game.player1:
+            raise endpoints.BadRequestException(
+                'Player #2 cannot be the same as Player #1.')
+
+        if game.player2:
+            raise endpoints.BadRequestException(
+                'Game is already full!')
+        try:
+            game.set_player2(user.key)
+        except ValueError as err:
+            raise endpoints.BadRequestException(err.message)
+
+        # Use a task queue to update the average attempts remaining.
+        # This operation is not needed to complete the creation of a new game
+        # so it is performed out of sequence.
+        #taskqueue.add(url='/tasks/cache_average_attempts')
+
+        return game.to_form('Good luck playing Tic Tac Toe!')
+
+    @endpoints.method(request_message=GET_GAME_REQUEST,
+                      response_message=GameForm,
+                      path='game/{urlsafe_game_key}',
+                      name='get_game',
+                      http_method='GET')
+    def get_game(self, request):
+        """Return the current game state."""
+        game = get_by_urlsafe(request.urlsafe_game_key, Game)
+        if game:
+                return game.to_form('Time to make a move!')
+        else:
+            raise endpoints.NotFoundException('Game not found!')
+
+    @endpoints.method(request_message=MAKE_MOVE_REQUEST,
+                      response_message=GameForm,
+                      path='game/{urlsafe_game_key}/move',
+                      name='make_move',
+                      http_method='PUT')
+    def make_move(self, request):
+        """Makes a move. Returns a game state with message"""
+
+        game = get_by_urlsafe(request.urlsafe_game_key, Game)
+
+        if game.game_over:
+            return game.to_form('Game already over!')
+
+        if not game.player2:
+            raise endpoints.NotFoundException('Waiting for player 2 to join.')
+
+        user = User.query(User.name == request.user_name).get()
+
+        if not user:
+            raise endpoints.NotFoundException('User "{}" does not exist.'
+                                              .format(request.user_name))
+
+        if user.key not in (game.player1, game.player2):
+            raise endpoints.BadRequestException('User "{}" not in this game.'
+                                                .format(request.user_name))
+
+        if game.next_turn != user.key:
+            raise endpoints.BadRequestException("It's not your turn!")
+
+        cell = getattr(game, 'cell_{}'.format(request.position))
+
+        if cell is None:
+            raise endpoints.BadRequestException('Cell #{} does not exist.'
+                                                .format(request.position))
+        elif cell != -1:
+            raise endpoints.BadRequestException('Cell #{} is already occupied.'
+                                                .format(request.position))
+
+        game.set_position(request.position, user.key)
+        game.next_turn = game.player2 if game.player1 == user.key else game.player1
+
+        symbol = TicTacToeApi.check_for_win(game=game)
+        if symbol > -1:
+            game.end_game(winner=user.key)
+            return game.to_form('You won!')
+
+        game.put()
+
+        return game.to_form()
+
+    @staticmethod
+    def check_for_win(game):
+        grid = game.grid
+
+        for i in range(3):
+            if grid[i][0] == grid[i][1] and grid[i][0] == grid[i][2]:
+                return grid[i][0]
+            if grid[0][i] == grid[1][i] and grid[0][i] == grid[2][i]:
+                return grid[0][i]
+
+        if grid[0][0] == grid[1][1] and grid[0][0] == grid[2][2]:
+            return grid[0][0]
+
+        if grid[2][0] == grid[1][1] and grid[2][0] == grid[0][2]:
+            return grid[2][0]
+
+        return -1
+
+    @endpoints.method(response_message=ScoreForms,
+                      path='scores',
+                      http_method='GET')
+    def get_scores(self, request):
+        """Return all scores"""
+        return ScoreForms(items=[score.to_form() for score in Score.query()])
+
+    @endpoints.method(request_message=USER_REQUEST,
+                      response_message=ScoreForms,
+                      path='scores/user/{user_name}',
+                      http_method='GET')
+    def get_user_scores(self, request):
+        """Returns all of an individual User's scores"""
+        user = User.query(User.name == request.user_name).get()
+        if not user:
+            raise endpoints.NotFoundException(
+                    'A User with that name does not exist!')
+        scores = Score.query(ancestor=user.key)
+        return ScoreForms(items=[score.to_form() for score in scores])
+
+    # @endpoints.method(response_message=StringMessage,
+    #                   path='games/average_attempts',
+    #                   name='get_average_attempts_remaining',
+    #                   http_method='GET')
+    # def get_average_attempts(self, request):
+    #     """Get the cached average moves remaining"""
+    #     return StringMessage(message=memcache.get(MEMCACHE_MOVES_REMAINING) or '')
+    #
+    # @staticmethod
+    # def _cache_average_attempts():
+    #     """Populates memcache with the average moves remaining of Games"""
+    #     games = Game.query(Game.game_over == False).fetch()
+    #     if games:
+    #         count = len(games)
+    #         total_attempts_remaining = sum([game.attempts_remaining
+    #                                         for game in games])
+    #         average = float(total_attempts_remaining)/count
+    #         memcache.set(MEMCACHE_MOVES_REMAINING,
+    #                      'The average moves remaining is {:.2f}'.format(average))
+
+    @endpoints.method(request_message=GET_USER_GAMES_REQUEST,
+                      response_message=GameForms,
+                      path='games/user/{user_name}',
+                      http_method='GET')
+    def get_user_games(self, request):
+        user = User.query(User.name == request.user_name).get()
+        if not user:
+            raise endpoints.NotFoundException('A User with that name does not exist!')
+
+        games = Game.query(Game.player1 == user.key).fetch()
+        if games:
+            return GameForms(items=[game.to_form() for game in games])
+        else:
+            raise endpoints.NotFoundException('User {} has not created any games.'
+                                              ''.format(request.user_name))
+
+    @endpoints.method(request_message=endpoints.ResourceContainer(urlsafe_game_key=messages.StringField(1)),
+                      response_message=StringMessage,
+                      path='game/{urlsafe_game_key}/cancel',
+                      http_method='PUT')
+    def cancel_game(self, request):
+        game = get_by_urlsafe(request.urlsafe_game_key, Game)
+
+        if not game:
+            raise endpoints.NotFoundException('That game does not exist!')
+
+        if game.game_over:
+            raise endpoints.BadRequestException('Game cannot be cancelled because it is already over.')
+        else:
+            game.cancel_game()
+
+        return StringMessage(message='Game cancelled.')
+
+    @endpoints.method(request_message=message_types.VoidMessage,
+                      response_message=RankingForms,
+                      path='user/rankings',
+                      http_method='GET')
+    def get_user_rankings(self, request):
+        # TODO: Fetch all Score entities first, then collate users from them.
+        # @ndb.tasklet
+        # def callback(user):
+        #     score = yield Score.query_user(user.key).fetch_async()
+        #     raise ndb.Return(score)
+
+        # scores = User.query().order(User.name).map(callback)
+
+        scores = Score.query().fetch()
+
+        performance = {}
+
+        for score in scores:
+
+            if score.winner in performance:
+                ranking = performance[score.winner]
+                ranking['wins'] += 1
+                ranking['avg_moves'] += (
+                    (score.winner_moves - ranking['avg_moves']) /
+                    ranking['wins'])
+            else:
+                performance[score.winner] = {
+                    'name': score.winner.get_async(), 'wins': 1, 'losses': 0,
+                    'avg_moves': score.winner_moves}
+
+            if score.loser in performance:
+                ranking = performance[score.loser]
+                ranking['losses'] += 1
+            else:
+                performance[score.loser] = {
+                    'name': score.get_loser_name(), 'wins': 0, 'losses': 1,
+                    'avg_moves': 0}
+
+        perf = sorted(performance.itervalues(),
+                      key=lambda x: (-x['wins'], -x['avg_moves']))
+
+        return RankingForms(
+            items=[RankingForm(user_name=p['name'].get_result(),
+                               rank=rank + 1,
+                               performance=self._win_loss_ratio(p['wins'],
+                                                                p['losses']))
+                   for rank, p in
+                   enumerate(perf)]
+        )
+
+    def _win_loss_ratio(self, wins, losses):
+        return wins / (wins + losses)
+
+# registers API
+api = endpoints.api_server([TicTacToeApi])
